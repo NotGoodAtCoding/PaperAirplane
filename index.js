4f1a2ed4d8c90a3f79d564f2554fee44a01ff8fc
Stefan Kraus <stefanakraus@gmail.com>
2016-10-09 14:22:37 -0500

    skele

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-i = 1;
 
-function inc() {
-    document.getElementById("example").innerHTML = (i++).toString();
-}
+(function render() {
+
+    // Put steps here
+
+    var instructions =
+        [
+            {
+                "title": "Zero indexed steps",
+                "image": "Doesn't do anything yet",
+                "body": [
+                    "Body paragraph goes here", "Add another for another paragraph"
+                ]
+            }
+        ];
+
+    var i=1;
+    var comp = "<div>";
+    instructions.forEach(function(instruction) {
+        comp += (
+          "<h3>" + "Step " + i + ": " + instruction.title + "</h3>"
+        );
+        instruction.body.forEach(function (paragraph){
+            comp += ("<p>" + paragraph + "</p>");
+        });
+    });
+    comp += "</div";
+    document.getElementById("example").innerHTML =
+        comp;
+})();
